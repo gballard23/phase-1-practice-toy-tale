18b4eec060b12a7e5691e7f54cd6e632e2e085ee
Gerren <g.ballard23@outlook.com>
2021-12-12 18:04:46 -0600

    update

--- src/index.js
+++ src/index.js
@@ -13,3 +13,43 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+let container = document.querySelector('#toy-collection');
+
+window.addEventListener('load', async function (){
+  console.log('its loaded');
+
+  let res = await fetch('http://localhost:3000/toys');
+  let data = await res.json();
+
+  console.log(data);
+
+  for(let i = 0; i < data.length; i++){
+    let newElement = document.createElement('div');
+    newElement.classList.add('card');
+
+    container.appendChild(newElement);
+
+    let h2 = document.createElement('h2');
+    h2.innerHTML = data[i].name;
+    newElement.appendChild(h2);
+
+    let img = document.createElement('img');
+    img.src = data[i].image;
+    newElement.appendChild(img);
+
+    let p = document.createElement('p');
+    p.innerHTML = data[i].likes;
+    newElement.appendChild(p);
+
+    let btn = document.createElement('button');
+    btn.innerHTML = 'Like';
+    newElement.appendChild(btn);
+
+    img.classList.add('toy-avatar');
+    btn.classList.add('like-btn');
+  }
+
+  
+
+})
